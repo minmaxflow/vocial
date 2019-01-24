95228f5a80d1e944dc298888c38789bd5d33d512
喻林 <yulin_yl@126.com>
2019-01-24 16:27:36 +0800

    fix typo

--- lib/vocial_web/controllers/api/error_controller.ex
+++ lib/vocial_web/controllers/api/error_controller.ex
@@ -4,7 +4,7 @@ defmodule VocialWeb.Api.ErrorController do
   def call(conn, {:error, :not_found}) do 
     conn 
     |> put_status(:not_found)
-    |> render(VocialWeb.ErrorView, "404,json")
+    |> render(VocialWeb.ErrorView, "404.json")
   end
 
   def call(conn, _) do 
